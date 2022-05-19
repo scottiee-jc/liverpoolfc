020b7f0ddb90be2030df72fe6a429e46d3fb2a91
Scottie Work <scottiework@Scotts-MacBook-Pro-2.local>
2022-05-19 14:53:30 +0100

    added more mapping

--- project/src/main/java/com/example/project/controllers/TeamController.java
+++ project/src/main/java/com/example/project/controllers/TeamController.java
@@ -1,9 +1,14 @@
 package com.example.project.controllers;
 
 import com.example.project.repository.TeamRepository;
+import model.Team;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("teams")
@@ -13,4 +18,19 @@ public class TeamController {
     TeamRepository teamRepository;
 
 
+    @GetMapping
+    public ResponseEntity<List<Team>> getAllTeams(){
+        return new ResponseEntity(teamRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Team>> getTeam(@PathVariable Long id){
+        return new ResponseEntity(teamRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<Team>> postEstate(@RequestBody Team team){
+        teamRepository.save(team);
+        return new ResponseEntity(teamRepository.findAll(), HttpStatus.CREATED);
+    }
 }
